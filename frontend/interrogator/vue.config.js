43bc668eae7d91a629e8392f386e6a79e194c7e4
mbongiovanni <m.bongiovanni@mongibello.tech>
2022-03-07 17:53:01 +0100

    docker-compose: improved frontend development envoirment, added hot reload functionality; Dockerfile_develop: added dockerfile in order to launch webpack integrated server for debug; vue.config.js: added reverse proxy rules;

--- frontend/interrogator/vue.config.js
+++ frontend/interrogator/vue.config.js
@@ -4,7 +4,19 @@ module.exports = {
   ],
   configureWebpack: {
     devServer: {
-      headers: {'Access-Control-Allow-Origin': '*' , 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE', 'Access-Control-Allow-Credentials': true}
+      headers: {'Access-Control-Allow-Origin': '*' , 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE', 'Access-Control-Allow-Credentials': true},
+      proxy: {
+        "/server/kill": {
+          target: 'http://server:3000/kill',
+          pathRewrite: {'^/server/kill' : ''},
+          changeOrigin: true
+        },
+        "/server/ping": {
+          target: 'http://server:3000/ping',
+          pathRewrite: {'^/server/ping' : ''},
+          changeOrigin: true
+        },
+    },
     }
   }
 }
